f58df6a183a1a4d4c12453968ac4f354d3363be2
Kahnu Charan Swain <djkanha044@gmail.com>
2023-07-16 16:54:23 +0530

    FIrst get request added

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 const PORT = 3001;
 
 
+app.get('/', (req, res)=>{
+    return res.json({
+        status : true,
+        message: "Welcome To Node Server"
+    });
+})
 
 app.listen(PORT, ()=>{
     console.log(`Node Server Started On Port ${PORT}`)
