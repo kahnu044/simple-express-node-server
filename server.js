123f9191b7d2b7fc873601559d3229d1f4be406d
Kahnu Charan Swain <djkanha044@gmail.com>
2023-07-16 16:55:17 +0530

    Code Refactor

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// Import the Express.js framework
 const express = require('express');
+
+// Create an instance of the Express application
 const app = express();
+
+// Port
 const PORT = 3001;
 
-
-app.get('/', (req, res)=>{
+// A route handler for the root URL ('/'). Responds to incoming GET requests with a JSON object containing a welcome message.
+app.get('/', (req, res) => {
     return res.json({
-        status : true,
+        status: true,
         message: "Welcome To Node Server"
     });
 })
 
-app.listen(PORT, ()=>{
+// Starts the Node.js server and listens on the specified port.
+app.listen(PORT, () => {
     console.log(`Node Server Started On Port ${PORT}`)
 });
